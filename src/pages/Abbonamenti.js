2c063ac9ab463cc0dfecd2a2778061c88b2256b9
Giuseppe Buscio <giuseppebuscio98@gmail.com>
2025-06-16 13:12:10 +0200

    Fix

--- src/pages/Abbonamenti.js
+++ src/pages/Abbonamenti.js
@@ -96,7 +96,7 @@ function Abbonamenti() {
                   WebkitBackgroundClip: 'text',
                   WebkitTextFillColor: 'transparent'
                 }}>
-                  I miei Abbonamentixxx
+                  I miei Abbonamenti
                 </h1>
                 <p style={{
                   fontSize: '1.125rem',
