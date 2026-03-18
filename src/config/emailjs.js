47332d6b2dfee505c38d6432e9aa6719780c24f7
Giuseppe <giuseppebuscio98@gmail.com>
2026-03-18 16:36:18 +0100

    configurazione emailjs

--- src/config/emailjs.js
+++ src/config/emailjs.js
@@ -2,7 +2,7 @@
 // Sostituisci questi valori con le tue credenziali EmailJS
 
 export const EMAILJS_CONFIG = {
-  SERVICE_ID: 'service_aruba_Nemo', // Da EmailJS Dashboard > Email Services
+  SERVICE_ID: 'service_google_Nemo', // Da EmailJS Dashboard > Email Services
   TEMPLATE_ID_QUOTE: 'template_9ec0b31', // Template per entrambi i form (contatti e preventivo)
   PUBLIC_KEY: 'BavXkDkgbZrizWYmm', // Da EmailJS Dashboard > Account > API Keys
 };
